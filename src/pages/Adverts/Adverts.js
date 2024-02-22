0e3fbe01265dc7d17a400394c3bd0c9f587fef82
Your Name <your-email@example.com>
2024-02-22 12:59:13 +0200

    add redux advertSlise

--- src/pages/Adverts/Adverts.js
+++ src/pages/Adverts/Adverts.js
@@ -1,86 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+// Adverts.js
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchAdvertsAsync,
+  setMake,
+  changeFavoriteList,
+} from '../../redux/Advert/advertSlice';
 
 import { CarList } from '../../components/CarList/CarList';
 import { Loader } from '../../components/helpers/Loader';
-import { fetchAdverts } from '../../Api';
 import { ButtonStyle } from './Adverts.style';
 import { SearchForm } from '../../components/SearchForm/SearchForm';
 
 const Adverts = () => {
-  const [arvertList, setArvertList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [page, setPage] = useState(1);
-  const [isLoadMoreHidden, setIsLoadMoreHidden] = useState(true);
-  const [favoriteList, setFavoriteList] = useState([]);
-  const [make, setMake] = useState('');
-
+  const dispatch = useDispatch();
+  const {
+    advertList,
+    isLoading,
+    isError,
+    page,
+    isLoadMoreHidden,
+    favoriteList,
+    make,
+  } = useSelector(state => state.advert);
   const isLogin = useSelector(state => state.user.isLogin);
 
   useEffect(() => {
-    async function getAdverts() {
-      setIsLoading(true);
-      setIsError(false);
-      try {
-        const result = await fetchAdverts(page);
-
-        setArvertList(prevList => [...prevList, ...result]);
-
-        setIsLoadMoreHidden(false);
-
-        if (result.length < 12) {
-          setIsLoadMoreHidden(true);
-        }
-      } catch (error) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    getAdverts();
-  }, [page]);
+    dispatch(fetchAdvertsAsync(page));
+  }, [dispatch, page]);
 
   const loadMore = () => {
-    setPage(prevPage => prevPage + 1);
+    dispatch(fetchAdvertsAsync(page + 1));
   };
 
-  const changeFavoriteList = advertId => {
-    setFavoriteList(prevList => {
-      const updatedList = new Set(prevList);
-
-      if (updatedList.has(advertId)) {
-        updatedList.delete(advertId);
-        console.log('Видалено', advertId);
-      } else {
-        updatedList.add(advertId);
-        console.log('Додано', advertId);
-      }
-
-      localStorage.setItem(
-        'favoriteList',
-        JSON.stringify(Array.from(updatedList))
-      );
-      return Array.from(updatedList);
-    });
+  const handleSetMake = value => {
+    dispatch(setMake(value));
   };
 
-  const visibleAdverts = arvertList.filter(advert =>
+  const handleChangeFavoriteList = advertId => {
+    dispatch(changeFavoriteList(advertId));
+  };
+
+  const visibleAdverts = advertList.filter(advert =>
     advert.make.toLowerCase().includes(make.toLowerCase())
   );
-  console.log(visibleAdverts);
 
   return (
     <div>
-      <SearchForm setMake={setMake} />
+      <SearchForm setMake={handleSetMake} />
       {isLoading && !isError && <Loader />}
 
       {visibleAdverts.length > 0 && (
         <CarList
           visibleAdverts={visibleAdverts}
           favoriteList={favoriteList}
-          changeFavoriteList={changeFavoriteList}
+          changeFavoriteList={handleChangeFavoriteList}
         />
       )}
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './Auth/userSlice';
+import { advertReducer } from './Advert/advertSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    advert: advertReducer,
   },
 });

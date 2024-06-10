147f7284a9f2f464edfabc339229586765b4f279
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2024-06-10 17:11:42 +0300

    changes

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,15 +1,12 @@
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
 import { signout } from '../../redux/Auth/operations';
-import { useAuth } from '../../hooks/useAuth';
 import { GoMoveToEnd } from 'react-icons/go';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { user } = useAuth();
   const handleSubmit = () => {
     dispatch(signout());
     navigate('/', { replace: true });

6928ec75293bcb4bbad5171bd38a53a19efbc7c5
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2024-08-09 14:38:06 +0300

    remove style Home-page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import SwiperSlider from '../../components/SwiperSlider/SwiperSlider.js';
 import { Title, Container } from './Home.style';
 const Home = () => {
   return (
-    <Container style={{ height: 'auto' }}>
+    <Container>
       <Title>
         Empower Your Journey: <br /> Seamless Car Rentals Tailored Just for You!
       </Title>
